636aff4f6f90cf25325ac724848539d6bfe6e125
Mayla Alves <mayla.alves@hotmail.com>
2021-04-21 16:27:11 -0300

    add funções para reiniciar os campos de texto

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,38 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $ (".tempo-digitacao").text();
 var campo = $ (".campo-digitacao");
-campo.on("input", function(){
-    var conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+//função do jQuery que chama as funções do JS qdo o documento html estiver carregado
+// função extendida: $(document).ready (function(){ abaixo função com atalho
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo)
 });
 
-var tempoRestante = $ (".tempo-digitacao").text();
-campo.one("focus", function(){
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores(){
+    campo.on("input", function(){
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}    
+
+function inicializaCronometro(){
+    var tempoRestante = $ (".tempo-digitacao").text();
+    campo.one("focus", function(){
     var cronometroID = setInterval(function(){
         tempoRestante--;
         $ (".tempo-digitacao").text(tempoRestante);
@@ -26,3 +42,20 @@ campo.one("focus", function(){
         }
     }, 1000);
 });
+}
+
+//$("#botao-reiniciar").on("click", function(){});
+//abaixo maneira função simplificada para zerar e destravar o campo de texto
+//$("#botao-reiniciar").click(function(){});
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $ ("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+};
+
+
+
