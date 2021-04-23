14b921d527844fbc901fd57d0b8055c9be672c68
Mayla Alves <mayla.alves@hotmail.com>
2021-04-23 07:48:21 -0300

    Adicionando eventos nas linhas do placar

--- public/js/main.js
+++ public/js/main.js
@@ -68,21 +68,43 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Seu-nome";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>" ;
 
-    var linha = "<tr>"+
+
+   /* var linha = "<tr>"+
                     "<td>"+ usuario + "</td>"+
                     "<td>"+ numPalavras + "</td>"+
                     "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
+                "</tr>"; */
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.prepend(linha);
 }
 
-$("#botao-remover").click (function(event){
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>");
+    var colunaUsuario =$("<td>").text(usuario);
+    var colunaPalavras =$("<td>").text(palavras);
+    var colunaRemover =$("<td>");
+
+    var link= $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone= $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(){
     event.preventDefault(); //previnir acontecimentos padrões
-    $(this).parent().parent().remove();
-});
+    $(this).parent() .parent() .remove();
+
+}
 
 //$("#botao-reiniciar").on("click", function(){});
 //abaixo maneira função simplificada para zerar e destravar o campo de texto
